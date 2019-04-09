70a8a51f348d5a84cd0c2110968b8f534ffc130e
Sehyn <48888351+Sehyn@users.noreply.github.com>
2019-04-10 01:55:38 +0200

    Add files via upload

---
+++ ClickNShare/MainForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ClickNShare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TakeScreenshotBtn = new System.Windows.Forms.Button();
+            this.LogTxtBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SetFolderPath = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TakeScreenshotBtn
+            // 
+            this.TakeScreenshotBtn.Location = new System.Drawing.Point(12, 12);
+            this.TakeScreenshotBtn.Name = "TakeScreenshotBtn";
+            this.TakeScreenshotBtn.Size = new System.Drawing.Size(386, 23);
+            this.TakeScreenshotBtn.TabIndex = 0;
+            this.TakeScreenshotBtn.Text = "Screenshot ";
+            this.TakeScreenshotBtn.UseVisualStyleBackColor = true;
+            this.TakeScreenshotBtn.Click += new System.EventHandler(this.TakeScreenshotBtn_Click);
+            // 
+            // LogTxtBox
+            // 
+            this.LogTxtBox.Enabled = false;
+            this.LogTxtBox.Location = new System.Drawing.Point(12, 90);
+            this.LogTxtBox.Multiline = true;
+            this.LogTxtBox.Name = "LogTxtBox";
+            this.LogTxtBox.ReadOnly = true;
+            this.LogTxtBox.Size = new System.Drawing.Size(386, 105);
+            this.LogTxtBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(386, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Set saving path";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Path:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "[!] Path not set";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 225);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LogTxtBox);
+            this.Controls.Add(this.TakeScreenshotBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "[#] Click N Share [#v.0.1]";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TakeScreenshotBtn;
+        private System.Windows.Forms.TextBox LogTxtBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog SetFolderPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ClickNShare/MainForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClickNShare
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Text = Properties.Settings.Default.SavingPath.ToString();
+        }
+
+        private async void TakeScreenshotBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                              Screen.PrimaryScreen.Bounds.Height,
+                              PixelFormat.Format32bppArgb);
+
+                var gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+
+                gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                Screen.PrimaryScreen.Bounds.Y,
+                                0,
+                                0,
+                                Screen.PrimaryScreen.Bounds.Size,
+                                CopyPixelOperation.SourceCopy);
+                LogTxtBox.AppendText("["+DateTime.Now.ToShortTimeString() +"] Captured whole monitor." + Environment.NewLine);
+                await Task.Delay(1000);
+                label2.Text = Properties.Settings.Default.SavingPath.ToString();
+                bmpScreenshot.Save(@label2.Text + "/Capture-" + DateTime.Now.ToString("yyyy-MM-dd-h-mm-tt") + ".png", ImageFormat.Png);
+                //Console.WriteLine(@label2.Text + "/Capture - " + DateTime.Now.ToString("yyyy - MM - dd - h - mm - tt") + ".png");
+
+                LogTxtBox.AppendText("[" + DateTime.Now.ToShortTimeString() + "] Saved screenshot under name : Capture-" + DateTime.Now.ToString("yyyy-MM-dd-h-mm-tt") + Environment.NewLine);
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Oopsie!" + ex.Message);
+            }
+
+
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = SetFolderPath.ShowDialog();
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(SetFolderPath.SelectedPath))
+            {
+                string Path = SetFolderPath.SelectedPath;
+                ClickNShare.Properties.Settings.Default.SavingPath = Path;
+                ClickNShare.Properties.Settings.Default.Save();
+                MessageBox.Show("Path set to: " + Path.ToString(), "Path set!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LogTxtBox.AppendText("[" + DateTime.Now.ToShortTimeString() + "] Path set to: " + Path.ToString() + Environment.NewLine);
+
+            }
+            else
+            {
+                MessageBox.Show("You must select a path in order to save the screenshots", "No path selected!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            //SetFolderPath.SelectedPath.ToString();
+        }
+    }
+}
